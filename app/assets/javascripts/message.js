2f4fc4c038bc45cf22fe74d309eadd9e29e423ed
sasaya7016 <sasaya7016@outlook.jp>
2020-03-19 19:08:21 +0900

    更新

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -78,29 +78,30 @@ $(function(){
       alert('メッセージ送信に失敗しました');
     })
   })
-    var reloadMessages = function() {
-      var last_message_id = $('.message:last').data("message-id");
-      $.ajax({
-        url: "api/messages",
-        type: 'get',
-        dataType: 'json',
-        data: {id: last_message_id}
-      })
-      .done(function(messages) {
-        if (messages.length !== 0) {
-          var insertHTML = '';
-          $.each(messages, function(i, message) {
-            insertHTML += buildHTML(message)
-          });
-          $('.message').append(insertHTML);
-          $('.message').animate({ scrollTop: $('.message')[0].scrollHeight});
-        }
-      })
-      .fail(function() {
-        alert('error');
-      });
-    };
-    if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-      setInterval(reloadMessages, 7000);
-    }
-  });
+  var reloadMessages = function() {
+    var last_message_id = $('.messages:last').data("message-id");
+    console.log(last_message_id)
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.message').append(insertHTML);
+        $('.message').animate({ scrollTop: $('.message')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
+});
